07874ad4c06bd4b518a96f389e4b87cea6a1d4cc
Riccardo Serafini <tapion@tapion.it>
2024-03-14 22:00:32 +0000

    rename rn24_backend to rn24

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,9 @@
-import os
 import multiprocessing
+import os
 
 workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:8000"
-wsgi_app = "rn24_backend.wsgi:application"
+wsgi_app = "rn24.wsgi:application"
 enable_stdio_inheritance = True
 
 if os.getenv("DJANGO_DEBUG", "").upper() in ("Y", "TRUE", "ON", "1"):
--- rn24/wsgi.py
+++ rn24/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rn24_backend project.
+WSGI config for rn24 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rn24_backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rn24.settings")
 
 application = get_wsgi_application()
