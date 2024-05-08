989ebcc26a496a48655d03b4c5adf93706dab771
Riccardo Serafini <tapion@tapion.it>
2024-05-08 11:56:02 +0000

    disable CsrfViewMiddleware

--- rn24/settings.py
+++ rn24/settings.py
@@ -93,7 +93,8 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # disabled for now
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
