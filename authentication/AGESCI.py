c35ec7d3112820d08eb7a5e2a3b5cfdd1d40a023
Riccardo Serafini <tapion@tapion.it>
2024-05-21 21:12:34 +0000

    don't fail if auth gateway request fails

--- authentication/AGESCI.py
+++ authentication/AGESCI.py
@@ -25,16 +25,20 @@ class AGESCILoginClient:
         # TODO: log error
 
     def login_AGESCI(self, username, password):
-        response = requests.post(
-            url=self.login_url,
-            json={"username": username, "password": password},
-            headers={"Authorization": f"Bearer {self.access_token}"},
-        )
-        if response.status_code == 200:
-            return (True, response.json())
-        if response.status_code == 401:
-            self.refresh_access_token()
-            # TODO: avoid endless loop
-            return self.login_AGESCI(username=username, password=password)
-        # TODO: log other errors
-        return (False, "Invalid credentials")
+        try:
+            response = requests.post(
+                url=self.login_url,
+                json={"username": username, "password": password},
+                headers={"Authorization": f"Bearer {self.access_token}"},
+            )
+            if response.status_code == 200:
+                return (True, response.json())
+            if response.status_code == 401:
+                self.refresh_access_token()
+                # TODO: avoid endless loop
+                return self.login_AGESCI(username=username, password=password)
+            # TODO: log other errors
+            return (False, "Invalid credentials")
+        except Exception:
+            # TODO: log error
+            return (False, "Invalid credentials")
