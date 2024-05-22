b49581bd1e5f5eea576d7a318eeefce1959c65fc
Riccardo Serafini <tapion@tapion.it>
2024-05-22 07:55:41 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-python manage.py migrate --noinput
+cd /opt/rn24-backend
+echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
+git pull origin master
+docker compose pull
+docker compose exec backend python manage.py migrate --noinput
+docker compose exec backend python manage.py collectstatic --noinput
+docker compose up -d
