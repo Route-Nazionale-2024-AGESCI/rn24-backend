a637f5c9dc978537e326a07fa5bad4967490ab0d
Riccardo Serafini <tapion@tapion.it>
2024-06-27 23:27:54 +0000

    gzip

--- rn24/settings.py
+++ rn24/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.gzip.GZipMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
